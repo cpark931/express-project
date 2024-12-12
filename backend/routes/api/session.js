8da84a82eeb06c459de35e1f8dc40aa6ab43899c
cpark <cpark931@gmail.com>
2024-12-11 19:39:54 -0800

    6th commit

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const router = express.Router();
 
 router.post(
     '/',
@@ -41,6 +42,29 @@ router.post(
     }
   );
 
-const router = express.Router();
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+  router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
 
 module.exports = router;
