e9a672e185af9eaf27e85bffc57777dcbe41a0cf
cpark <cpark931@gmail.com>
2025-01-13 16:48:03 -0800

    fixed backend stuff

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,14 +1,13 @@
-//^ backend/utils/auth.js
+// backend/utils/auth.js
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
 const { User } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
-//~ This function will be used in the login and signup routes
-//! Sends a JWT Cookie
+// Sends a JWT Cookie
 const setTokenCookie = (res, user) => {
-    //! Create the token.
+    // Create the token.
     const safeUser = {
       id: user.id,
       email: user.email,
@@ -17,14 +16,14 @@ const setTokenCookie = (res, user) => {
     const token = jwt.sign(
       { data: safeUser },
       secret,
-      { expiresIn: parseInt(expiresIn) } //! 604,800 seconds = 1 week
+      { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
     );
 
     const isProduction = process.env.NODE_ENV === "production";
 
-    //! Set the token cookie
+    // Set the token cookie
     res.cookie('token', token, {
-      maxAge: expiresIn * 1000, //! maxAge in milliseconds
+      maxAge: expiresIn * 1000, // maxAge in milliseconds
       httpOnly: true,
       secure: isProduction,
       sameSite: isProduction && "Lax"
@@ -33,40 +32,36 @@ const setTokenCookie = (res, user) => {
     return token;
   };
 
-//~ The restoreUser middleware will be connected to the API router
-//~ so that all API route handlers will check if there is a current user logged in or not.
-const restoreUser = (req, res, next) => {
-//! token parsed from cookies
-const { token } = req.cookies;
-req.user = null;
+  const restoreUser = (req, res, next) => {
+    // token parsed from cookies
+    const { token } = req.cookies;
+    req.user = null;
 
-return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-    if (err) {
-    return next();
-    }
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+      if (err) {
+        return next();
+      }
 
-    try {
-    const { id } = jwtPayload.data;
-    req.user = await User.findByPk(id, {
-        attributes: {
-        include: ['email', 'createdAt', 'updatedAt']
-        }
+      try {
+        const { id } = jwtPayload.data;
+        req.user = await User.findByPk(id, {
+          attributes: {
+            include: ['email', 'createdAt', 'updatedAt']
+          }
+        });
+      } catch (e) {
+        res.clearCookie('token');
+        return next();
+      }
+
+      if (!req.user) res.clearCookie('token');
+
+      return next();
     });
-    } catch (e) {
-    res.clearCookie('token');
-    return next();
-    }
+  };
 
-    if (!req.user) res.clearCookie('token');
-
-    return next();
-  });
-};
-
-//~ requireAuth will be connected directly to route handlers where there needs to be a
-//~ current user logged in for the actions in those route handlers.
-//! If there is no current user, return an error
-const requireAuth = function (req, _res, next) {
+// If there is no current user, return an error
+    const requireAuth = function (req, _res, next) {
     if (req.user) return next();
 
     const err = new Error('Authentication required');
@@ -74,25 +69,6 @@ const requireAuth = function (req, _res, next) {
     err.errors = { message: 'Authentication required' };
     err.status = 401;
     return next(err);
-}
-
-//~ ownership aka authorization middleware
-const checkOwnership = (model, paramId, ownershipField = 'ownerId') => {
-  return async (req, res, next) => {
-    const id  =  req.params[paramId];
-    const resource = await model.findByPk(id);
-
-    if (!resource) {
-      return res.status(404).json({ message: `${model.name} couldn't be found` });
-    };
-
-    if (resource[ownershipField] !== req.user.id) {
-      return res.status(403).json({ message: `Forbidden` })
-    };
-
-    next();
   };
-};
 
-
-module.exports = { setTokenCookie, restoreUser, requireAuth, checkOwnership };
+  module.exports = { setTokenCookie, restoreUser, requireAuth };
