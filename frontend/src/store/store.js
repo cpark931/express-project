b9082dde2df38c4ab0a1d96c568413f704dba86e
cpark <cpark931@gmail.com>
2025-02-21 11:49:22 -0800

    troubleshooting

---
+++ frontend/src/store/store.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import sessionReducer from './session';
+import spotsReducer from './spot';
+import reviewsReducer from './review';
+
+const rootReducer = combineReducers({
+  // ADD REDUCERS HERE
+  session: sessionReducer,
+  spots: spotsReducer,
+  reviews: reviewsReducer
+});
+
+
+let enhancer;
+if (import.meta.env.MODE === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
